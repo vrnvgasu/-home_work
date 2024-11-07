95519df755ee93eebec6a06dca3d7d1f29653ff4
Dmitrii <dmitrii@psb.com>
2024-11-07 22:52:50 +0300

    HW9 is completed

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,12 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"slices"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,11 +17,11 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
@@ -39,13 +43,105 @@ type (
 func TestValidate(t *testing.T) {
 	tests := []struct {
 		in          interface{}
-		expectedErr error
+		expectedErr ValidationErrors
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    18,
+				Email:  "test@test.com",
+				Role:   "stuff",
+				Phones: []string{"79001001010", "79002002020"},
+				meta:   json.RawMessage(`{"number":123}`),
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "1",
+				Name:   "test",
+				Age:    10,
+				Email:  "test@testcom",
+				Role:   "dummy",
+				Phones: []string{"555", "222"},
+				meta:   json.RawMessage(nil),
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "ID",
+					Err:   errLen,
+				},
+				ValidationError{
+					Field: "Age",
+					Err:   errMin,
+				},
+				ValidationError{
+					Field: "Email",
+					Err:   errRegexp,
+				},
+				ValidationError{
+					Field: "Role",
+					Err:   errIn,
+				},
+				ValidationError{
+					Field: "Phones",
+					Err:   errLen,
+				},
+				ValidationError{
+					Field: "Phones",
+					Err:   errLen,
+				},
+			},
+		},
+		{
+			in: App{
+				Version: "12345",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: App{
+				Version: "",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Version",
+					Err:   errLen,
+				},
+			},
+		},
+		{
+			in:          Token{},
+			expectedErr: nil,
+		},
+		{
+			in: Token{
+				Header:    []byte("Header"),
+				Payload:   []byte("Payload"),
+				Signature: []byte("Signature"),
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 404,
+				Body: "not found",
+			},
+			expectedErr: nil,
+		},
+		{
+			in: Response{
+				Code: 201,
+				Body: "",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "Code",
+					Err:   errIn,
+				},
+			},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +149,163 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+
+			if tt.expectedErr == nil {
+				require.NoError(t, err)
+
+				return
+			}
+
+			var vErrs ValidationErrors
+			ok := errors.As(err, &vErrs)
+			require.True(t, ok)
+
+			slices.SortFunc(vErrs, func(a, b ValidationError) int {
+				if a.Field > b.Field {
+					return 1
+				}
+
+				return -1
+			})
+			slices.SortFunc(tt.expectedErr, func(a, b ValidationError) int {
+				if a.Field > b.Field {
+					return 1
+				}
+
+				return -1
+			})
+
+			require.Equal(t, tt.expectedErr, vErrs)
+		})
+	}
+}
+
+func TestValidateLangError(t *testing.T) {
+	tests := []struct {
+		in          interface{}
+		expectedErr require.ErrorAssertionFunc
+	}{
+		{
+			in: struct {
+				ID     string `json:"id" validate:"len:36"`
+				Name   string
+				Age    int      `validate:"min:18|max:50"`
+				Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   UserRole `validate:"in:admin,stuff"`
+				Phones []string `validate:"len:11"`
+			}{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    18,
+				Email:  "test@test.com",
+				Role:   "stuff",
+				Phones: []string{"79001001010", "79002002020"},
+			},
+			expectedErr: require.NoError,
+		},
+		{
+			in: struct {
+				ID     int `json:"id" validate:"len:36"`
+				Name   string
+				Age    int      `validate:"min:18|max:50"`
+				Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   UserRole `validate:"in:admin,stuff"`
+				Phones []string `validate:"len:11"`
+			}{
+				ID:     123,
+				Name:   "test",
+				Age:    18,
+				Email:  "test@test.com",
+				Role:   "stuff",
+				Phones: []string{"79001001010", "79002002020"},
+			},
+			expectedErr: require.Error,
+		},
+		{
+			in: struct {
+				ID     string `json:"id" validate:"len:36"`
+				Name   string
+				Age    string   `validate:"min:18|max:50"`
+				Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   UserRole `validate:"in:admin,stuff"`
+				Phones []string `validate:"len:11"`
+			}{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    "18",
+				Email:  "test@test.com",
+				Role:   "stuff",
+				Phones: []string{"79001001010", "79002002020"},
+			},
+			expectedErr: require.Error,
+		},
+		{
+			in: struct {
+				ID     string `json:"id" validate:"len:36"`
+				Name   string
+				Age    int      `validate:"min:18|max:50"`
+				Email  int      `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   UserRole `validate:"in:admin,stuff"`
+				Phones []string `validate:"len:11"`
+			}{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    18,
+				Email:  99,
+				Role:   "stuff",
+				Phones: []string{"79001001010", "79002002020"},
+			},
+			expectedErr: require.Error,
+		},
+		{
+			in: struct {
+				ID     string `json:"id" validate:"len:36"`
+				Name   string
+				Age    int      `validate:"min:18|max:50"`
+				Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   bool     `validate:"in:admin,stuff"`
+				Phones []string `validate:"len:11"`
+			}{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    18,
+				Email:  "test@test.com",
+				Role:   true,
+				Phones: []string{"79001001010", "79002002020"},
+			},
+			expectedErr: require.Error,
+		},
+		{
+			in: struct {
+				ID     string `json:"id" validate:"len:36"`
+				Name   string
+				Age    int      `validate:"min:18|max:50"`
+				Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+				Role   UserRole `validate:"in:admin,stuff"`
+				Phones []int    `validate:"len:11"`
+			}{
+				ID:     "3422b448-2460-4fd2-9183-8000de6f8343",
+				Name:   "test",
+				Age:    18,
+				Email:  "test@test.com",
+				Role:   "stuff",
+				Phones: []int{555, 777},
+			},
+			expectedErr: require.Error,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Validate(tt.in)
+			tt.expectedErr(t, err)
+
+			var vErrs ValidationErrors
+			require.False(t, errors.As(err, &vErrs))
 		})
 	}
 }
